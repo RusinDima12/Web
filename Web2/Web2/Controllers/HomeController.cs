1915f14f3d9ee4cd0e60ee851d0a03fe83a949dc
Rusin Dima <rusin199812@mail.ru>
2017-05-14 21:53:24 +0700

    Начало

---
+++ Web2/Web2/Controllers/HomeController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web2.Models;
+
+namespace Web2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = ".";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Forum()
+        {
+            ViewBag.Message = "Форум";
+
+            return View();
+        }
+        public ActionResult Subject()
+        {
+            ViewBag.Message = "Выберите предмет";
+
+            return View();
+        }
+        public ActionResult MainPagehysic()
+        {
+            //ViewBag.Message = "Выберите предмет";
+
+            return View();
+        }
+        [Authorize(Roles = "admin")]
+        public ActionResult EditingIndex()
+        {
+            ViewBag.Message = "Изменение главной страницы";
+
+            return View();
+        }
+        [Authorize(Roles = "admin")]
+        public ActionResult EditingIndex1()
+        {
+            ViewBag.Message = "Изменение главной страницы";
+
+            return View();
+        }
+        [Authorize(Roles = "admin")]
+        public ActionResult EditingAbout()
+        {
+            ViewBag.Message = "Изменение информации института";
+
+            return View();
+        }
+        [Authorize(Roles = "admin")]
+        public ActionResult EditingContact()
+        {
+            ViewBag.Message = "Изменение контактов";
+
+            return View();
+        }
+        [Authorize(Roles = "admin")]
+        public ActionResult EditingForum()
+        {
+            ViewBag.Message = "Изменение содержимого форума";
+
+            return View();
+        }
+        [Authorize(Roles = "admin")]
+        public ActionResult EditingSubject()
+        {
+            ViewBag.Message = "Изменение содержимого предметов";
+
+            return View();
+        }
+        public ActionResult Mathematic()
+        {
+            ViewBag.Message = "Математика";
+
+            return View(ComCont.CommentModels);
+        }
+        public ActionResult InformationIndex()
+        {
+            ViewBag.Message = "Информатика";
+            return View();
+        }
+        public ActionResult InformationLesson1()
+        {
+            return View();
+        }
+        private CommentContext ComCont = new CommentContext();
+        public ActionResult Create()
+        {
+            ViewBag.Message = "Информатика";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CommentModel Com, HttpPostedFileBase uploadImage)
+        {
+            if (ModelState.IsValid && uploadImage != null)
+            {
+                //byte[] imageData = null;
+                //// считываем переданный файл в массив байтов
+                //using (var binaryReader = new BinaryReader(uploadImage.InputStream))
+                //{
+                //    imageData = binaryReader.ReadBytes(uploadImage.ContentLength);
+                //}
+                //// установка массива байтов
+                //pic.Image = imageData;
+
+                ComCont.CommentModels.Add(Com);
+                ComCont.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(Com);
+        }
+    }
+}
---
+++ Web2/Web2/Models/CommentModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Web2.Models
+{
+    public class CommentModel
+    {
+        [Required(ErrorMessage = "Не указано имя")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 50 символов")]
+        [Display(Name = "Имя")]            
+        public string Name { get; set; }
+
+       [Required(ErrorMessage = "Вы ничего не ввели")]            
+       [Display(Name = "Комментарий")]
+       public string Text{ get; set; }       
+    }
+
+    public class CommentContext : DbContext
+    {
+        public CommentContext()
+            :base("DefaultConnection (Web2)")
+        { }
+        public DbSet<CommentModel> CommentModels { get; set; }
+    }
+}
